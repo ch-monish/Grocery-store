4003b7ed6453d046d9f8366841cdb1ad001e4296
monish-cheruku <scheruku@belcan.com>
2022-11-12 11:53:33 +0530

    Initialize project using Create React App

---
+++ pages/homePage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header'
+import NavBar from '../../components/NavBar'
+
+function HomePage() {
+  return (
+    <div>
+        <NavBar></NavBar>
+        <Header></Header>
+    </div>
+  )
+}
+
+export default HomePage
